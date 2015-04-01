2b31cf5f98209c7815bc38fc2d376220a2c326dc
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-04-01 17:53:07 +0100

    Add geofence initialization

--- src/www/js/geofences.js
+++ src/www/js/geofences.js
@@ -1,5 +1,13 @@
 'use strict';
 
 define(function() {
+    var geofence = window.geofence;
 
+    if (geofence === undefined) {
+        console.error('Geofence plugin not installed');
+        console.error('Please include com.cowbell.cordova.geofence in your project');
+        return;
+    }
+
+    geofence.initialize();
 });
