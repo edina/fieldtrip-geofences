9c25b3b3ecefc02f056e334dbc508f52b4a1a578
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-04-09 15:18:06 +0100

    Get the geofences when the editor is downloaded
    Issue edina/fieldtrip-gb#173

--- src/www/js/geofences.js
+++ src/www/js/geofences.js
@@ -1,13 +1,57 @@
 'use strict';
 
-define(function() {
+define(function(require) {
+    var records = require('records');
+    var utils = require('./utils');
     var geofence = window.geofence;
 
+    // Check for the presence of the cordova plugin
     if (geofence === undefined) {
-        console.error('Geofence plugin not installed');
-        console.error('Please include com.cowbell.cordova.geofence in your project');
+        console.warn('Geofence plugin not installed');
+        console.warn('Please include com.cowbell.cordova.geofence in your project');
         return;
     }
 
+    // Initialize cordova plugin
     geofence.initialize();
+
+    /**
+     * Extract the geofences from the editor markup
+     * implements the records.processEditor interface
+     * @param editorName name of the editor
+     * @param html html content of the editor
+     * @param group from records.EDITOR_GROUP
+     * @param online boolean value if the processing is held online
+     */
+    var extractGeofences = function(editorName, html, group, online) {
+        var $form = $(html);
+        var geofence;
+        var geofences;
+
+        $form
+            .find('[data-geofences]')
+            .each(function(i, el) {
+                var geofenceFile;
+                var geofenceName;
+
+                geofenceFile = $(el).data('geofences');
+                geofenceName = geofenceFile.match(/(.*)\.json$/)[1];
+                geofence = {};
+                geofence[geofenceName] = geofenceFile;
+
+                geofences = utils.getLocalItem('geofences');
+                // Initialize the geofences as empty object if null
+                if (geofences === null) {
+                    geofences = {};
+                }
+
+                geofences[editorName] = geofences[editorName] || [];
+                geofences[editorName].push(geofence);
+
+                utils.setLocalItem('geofences', geofences);
+            });
+    };
+
+    // Add the plugin editor process to the pipeline
+    records.addProcessEditor(extractGeofences);
 });
